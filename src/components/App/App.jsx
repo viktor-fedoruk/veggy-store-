7e9899cab535a3c1920e8dfdd61a55624e9c1150
Viktor <victorfedoruk@gmail.com>
2023-09-07 11:34:11 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import useGetProducts from "../hooks/useGetProducts.js";
-import Header from './components/Header/Header.jsx';
-import ProductList from './components/Product/ProductList/ProductList.jsx';
-import Spinner from "./components/Spinner/Spinner.jsx";
+import useGetProducts from "./useGetProducts.js";
+import Header from '../Header/Header.jsx';
+import ProductList from '../Product/ProductList/ProductList.jsx';
+import Spinner from "../Spinner/Spinner.jsx";
 import './App.css';
-import './components/Header/HeaderCart/CartList/CartList.css';
+import '../Header/HeaderCart/CartList/CartList.css';
 
 export default function App() {
     const [isCartShakingAnimation, setIsCartShakingAnimation] = useState(false);
